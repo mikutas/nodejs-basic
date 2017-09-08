e17f589c4a58a65fd76fda43b630cf82d7e0961e
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2017-09-08 16:39:06 +0900

    レクチャー12完了

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
-var user = require("./blog/user");
-var post = require("./blog/post");
+var http = require("http");
+var server = http.createServer();
 
-user.createUser();
-post.createPost();
+var url = require("url");
+
+server.on("request", function(req, res){
+	//console.log("request caught");
+    console.log(req.url); 
+    var incomingUrl = url.parse(req.url);
+    console.log(incomingUrl);
+
+    if (incomingUrl.path === "/hello"){
+        res.writeHead(200);
+        res.end("Hello, world!");
+    }
+    else if (incomingUrl.path === "/goodbye"){
+        res.writeHead(200);
+        res.end("Good-bye, world!");
+    }
+    else {
+        res.writeHead(200);
+        res.end("Resource not found.");
+    }
+});
+
+server.listen(30000);
 
